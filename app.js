24436c885ea924d403a69bd7998ae908c6b317fe
Muhammad Hammad <letstalkhammad@gmail.com>
2023-11-07 14:45:53 +0500

    add some files

---
+++ app.js
@@ -0,0 +1,30 @@
+function email(){
+    window.location.href = "mailto:talktohammad@goutlook.com"
+}
+function linkedin(){
+    window.location.href = "https://www.linkedin.com/in/hammad-profile/";
+}
+function github(){
+    window.location.href = "https://github.com/hammad-official";
+}
+function whatsapp(){
+    window.location.href = "https://wa.me/923408659016";
+}
+function discord(){
+    window.location.href = "https://discord.com/channels/hammad_profile";
+}
+function instagram(){
+    window.location.href = "https://instagram.com/talktohammad";
+}
+function twitter(){
+    window.location.href = "https://twitter.com/talktohammad";
+}
+function snapchat(){
+    window.location.href = "https://www.snapchat.com/talktohammad/talktohammad";
+}
+function facebook(){
+    window.location.href = "https://www.facebook.com/talktohammad";
+}
+
+
+
